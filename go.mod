76d27eef19cc8b4bb47b50e8013f3bf10e7819e7
snoredude <snoredude@aliyun.com>
2022-07-27 20:21:23 +0800

    feat: init project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/snoredude/trial_workspaces_utils
+
+go 1.18
---
+++ utils.go
@@ -0,0 +1,7 @@
+package utils
+
+import "fmt"
+
+func SayHello(s string) string {
+	return fmt.Sprintf("Hello, %s", s)
+}
